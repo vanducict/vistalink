b7d26d270082fbb75f1afd3d115c2f4a6599db62
vanducict <vanducict@outlook.com>
2025-01-02 16:46:41 +0100

    change auth to supabase rework home

--- service/user/UserService.js
+++ service/user/UserService.js
@@ -28,15 +28,37 @@ export const insertUser = async (user, email, name, firstName, birthdate) => {
 
 
 export const getCurrentUser = async () => {
-    const {data: user, error} = await supabase
-        .from('User')
-        .select('*')
-    if (error) {
-        console.log("Error fetching user:", error);
+    try {
+
+        const {data: {session}, error: sessionError} = await supabase.auth.getSession();
+
+        if (sessionError) {
+            console.error("Error fetching session:", sessionError);
+            return null;
+        }
+        
+        if (!session?.user) {
+            console.log("No active session found");
+            return null;
+        }
+
+
+        const {data: user, error: userError} = await supabase
+            .from('User')
+            .select('*')
+            .eq('email', session.user.email);
+
+        // If there's an error fetching user data
+        if (userError) {
+            console.error("Error fetching user data:", userError);
+            return null;
+        }
+
+        return user;
+    } catch (e) {
+        console.error("Unexpected error:", e);
         return null;
     }
-
-    return user;
 }
 
 
