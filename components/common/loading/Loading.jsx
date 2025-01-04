2a5b66adb1b4efdfa752cb0d0e02909bfe58b5a5
vanducict <vanducict@outlook.com>
2025-01-04 17:59:58 +0100

    fix loading screen

--- components/common/loading/Loading.jsx
+++ components/common/loading/Loading.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import animations from "../../../constants/animations";
 import Lottie from "lottie-react-native";
-
+import {View} from "react-native";
+import styles from "./Loading.style";
 
 const Loading = ({loading}) => {
     return (loading ?
         (
-            <Lottie
-                source={animations.loading}
-                autoPlay
-                loop
-                style={{width: 50, height: 50}}
-            />
+            <View style={styles.loadingIndicator}>
+                <Lottie
+
+                    source={animations.loading}
+                    autoPlay
+                    loop
+                    style={{width: 50, height: 50}}
+                />
+            </View>
+
         ) : null)
 };
 
